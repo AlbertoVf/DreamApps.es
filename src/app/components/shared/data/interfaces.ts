1b0a085f3f7d482f597381d94f33b595a80d94b4
AlbertoVf <8244709-AlbertoVf1@users.noreply.gitlab.com>
2021-03-22 19:50:00 +0100

    Actualizacion de item-data

--- src/app/components/shared/data/interfaces.ts
+++ src/app/components/shared/data/interfaces.ts
@@ -8,16 +8,3 @@ export interface ISubmenu {
     imagen: String;
     texto: String[];
 }
-
-export class Item implements ISubmenu {
-    id: number;
-    submenu: String = "";
-    imagen: String = `../../../../ assets / img / items / ${this.submenu}.png`;
-    texto: String[];
-
-    constructor(id: number, submenu: String, texto: String[]) {
-        this.id = id;
-        this.submenu = submenu;
-        this.texto = texto;
-    }
-}
--- src/app/components/shared/item-data/item-data.component.ts
+++ src/app/components/shared/item-data/item-data.component.ts
@@ -1,16 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { Item } from '../data/interfaces';
+import { Component, Input, OnInit } from '@angular/core';
+import { ISubmenu } from '../data/interfaces';
 
 @Component({
     selector: 'app-item-data',
     templateUrl: './item-data.component.html',
-    styleUrls: ['./item-data.component.css']
+    styleUrls: ['./item-data.component.css'],
 })
 export class ItemDataComponent implements OnInit {
-    items: Item;
-    constructor() { }
+    constructor() {}
 
-    ngOnInit(): void {
-    }
+    ngOnInit(): void {}
 
+    @Input() items: ISubmenu[];
 }
