76c302d6eab6c32ceac0faa48cc6e1d83e54efdc
lexAmne <80521588+lexAmne@users.noreply.github.com>
2021-03-23 10:09:07 +0100

    modificaciones menu

--- src/app/components/static/header/header.component.js
+++ src/app/components/static/header/header.component.js
@@ -29,7 +29,7 @@
 
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('writting-navigation-menu writting-close');
           $('.mobile-nav-overly').fadeOut();
         }
         return false;
@@ -56,12 +56,12 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="writting-navigation-menu"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function(e) {
       $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-toggle i').toggleClass('writting-navigation-menu writting-close');
       $('.mobile-nav-overly').toggle();
     });
 
@@ -76,7 +76,7 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('writting-navigation-menu writting-close');
           $('.mobile-nav-overly').fadeOut();
         }
       }
